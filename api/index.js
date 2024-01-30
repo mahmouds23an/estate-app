dee7c9353f746209e7509620f2de0dd1d51df8dc
mahmoud <pgemahmoud@gmail.com>
2024-01-30 20:01:08 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
 const app = express();
 
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to database successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(3000, () => {
   console.log("API Working...");
 });
