e78b2dbcd5d8f2272cc0612f98ea34c438cc16c1
mahmoud <pgemahmoud@gmail.com>
2024-01-30 19:05:39 +0200

    Create header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import Header from "../components/Header.jsx";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Header />
+      <div>Home</div>
+    </>
+  );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,10 @@
+import Header from "../components/Header";
 
 export default function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <>
+      <Header />
+      <div>Profile</div>
+    </>
+  );
 }
